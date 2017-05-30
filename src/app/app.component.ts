d3bda2842f9b88ffe80e4db9eedc18239e347c48
Ali Rasheed <rasheed.ali22@gmail.com>
2017-05-29 22:14:45 -0500

    disable back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,9 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      platform.registerBackButtonAction(()=> {
+
+      },);
 
       userService.hasLoggedIn().then(data => {
         if (data) this.rootPage = TabsPage;
