3fc1d0ec3248e8902562a3feb8907e7bf8aedc29
kre8tive <abdullah@kre8tive.co>
2017-07-09 15:29:57 -0500

    app updated and ready for submit

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,21 +1,41 @@
-import { Component, ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ModalController, NavParams, NavController, Slides } from 'ionic-angular';
+import { Push, PushToken } from '@ionic/cloud-angular';
+
+// components
+import { TabsPage } from "../tabs/tabs";
 import { UserService } from "../core/user.service";
 
 
 @Component({
   selector: 'page-onboarding',
   templateUrl: 'onboarding.component.html',
-  providers: [ UserService ]
+  providers: [UserService]
 })
 export class OnBoardingComponent {
 
-  constructor(public modalControler: ModalController, public navCtrl: NavController,private userService: UserService) {}
+  constructor(
+    public modalControler: ModalController,
+    public navCtrl: NavController,
+    public push: Push,
+    private userService: UserService) { }
   /**
    * @name presentLoginModal
    * @description Presents the Email Capture Modal
    */
-  presentApp(){
-    this.userService.login();
+  presentApp() {
+    this.push.register()
+                  .then((t: PushToken) => {
+                    return this.push.saveToken(t);
+                  })
+                  .then((t: PushToken) => {
+                    this.userService.login();
+                    console.log('Token saved', t.token);
+                    this.navCtrl.push(TabsPage);
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                    console.error('push token not saved');
+                  });
   }
 }
